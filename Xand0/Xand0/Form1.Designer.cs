3fcb70bf9a9a3829eeefd6c638a364b9fc13ab73
SolomiyaSynytsya <solomiyasynytsya@ukr.net>
2016-08-22 13:37:26 +0300

    Added all files

---
+++ Xand0/Xand0/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Xand0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox textBox2;
+            System.Windows.Forms.TextBox textBox3;
+            System.Windows.Forms.TextBox textBox4;
+            System.Windows.Forms.TextBox textBox5;
+            System.Windows.Forms.TextBox textBox6;
+            System.Windows.Forms.TextBox textBox7;
+            System.Windows.Forms.TextBox textBox8;
+            System.Windows.Forms.TextBox textBox9;
+            System.Windows.Forms.TextBox textBox1;
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            textBox4 = new System.Windows.Forms.TextBox();
+            textBox5 = new System.Windows.Forms.TextBox();
+            textBox6 = new System.Windows.Forms.TextBox();
+            textBox7 = new System.Windows.Forms.TextBox();
+            textBox8 = new System.Windows.Forms.TextBox();
+            textBox9 = new System.Windows.Forms.TextBox();
+            textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 59);
+            this.label1.MinimumSize = new System.Drawing.Size(153, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 24);
+            this.label1.TabIndex = 9;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(91, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 41);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox2.HideSelection = false;
+            textBox2.Location = new System.Drawing.Point(195, 207);
+            textBox2.Margin = new System.Windows.Forms.Padding(1);
+            textBox2.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox2.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(50, 50);
+            textBox2.TabIndex = 12;
+            textBox2.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox3.HideSelection = false;
+            textBox3.Location = new System.Drawing.Point(143, 207);
+            textBox3.Margin = new System.Windows.Forms.Padding(1);
+            textBox3.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox3.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(50, 50);
+            textBox3.TabIndex = 13;
+            textBox3.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox4.HideSelection = false;
+            textBox4.Location = new System.Drawing.Point(143, 155);
+            textBox4.Margin = new System.Windows.Forms.Padding(1);
+            textBox4.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox4.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(50, 50);
+            textBox4.TabIndex = 16;
+            textBox4.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox5.HideSelection = false;
+            textBox5.Location = new System.Drawing.Point(195, 155);
+            textBox5.Margin = new System.Windows.Forms.Padding(1);
+            textBox5.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox5.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(50, 50);
+            textBox5.TabIndex = 15;
+            textBox5.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox6.HideSelection = false;
+            textBox6.Location = new System.Drawing.Point(91, 207);
+            textBox6.Margin = new System.Windows.Forms.Padding(1);
+            textBox6.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox6.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new System.Drawing.Size(50, 50);
+            textBox6.TabIndex = 14;
+            textBox6.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox7.HideSelection = false;
+            textBox7.Location = new System.Drawing.Point(143, 103);
+            textBox7.Margin = new System.Windows.Forms.Padding(1);
+            textBox7.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox7.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new System.Drawing.Size(50, 50);
+            textBox7.TabIndex = 19;
+            textBox7.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox8.HideSelection = false;
+            textBox8.Location = new System.Drawing.Point(195, 103);
+            textBox8.Margin = new System.Windows.Forms.Padding(1);
+            textBox8.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox8.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new System.Drawing.Size(50, 50);
+            textBox8.TabIndex = 18;
+            textBox8.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox9
+            // 
+            textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox9.HideSelection = false;
+            textBox9.Location = new System.Drawing.Point(91, 155);
+            textBox9.Margin = new System.Windows.Forms.Padding(1);
+            textBox9.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox9.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new System.Drawing.Size(50, 50);
+            textBox9.TabIndex = 17;
+            textBox9.Click += new System.EventHandler(this.clc);
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox1.HideSelection = false;
+            textBox1.Location = new System.Drawing.Point(91, 103);
+            textBox1.Margin = new System.Windows.Forms.Padding(1);
+            textBox1.MaximumSize = new System.Drawing.Size(50, 50);
+            textBox1.MinimumSize = new System.Drawing.Size(50, 50);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(50, 50);
+            textBox1.TabIndex = 20;
+            textBox1.Click += new System.EventHandler(this.clc);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 333);
+            this.Controls.Add(textBox1);
+            this.Controls.Add(textBox7);
+            this.Controls.Add(textBox8);
+            this.Controls.Add(textBox9);
+            this.Controls.Add(textBox4);
+            this.Controls.Add(textBox5);
+            this.Controls.Add(textBox6);
+            this.Controls.Add(textBox3);
+            this.Controls.Add(textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Xand0/Xand0/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Xand0
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Click any square";
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            restart();
+            i = 0;
+        }
+        private void clc(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            TextBox clickedTextBox = sender as TextBox;
+            clickedTextBox.Text = "X";
+            clickedTextBox.Enabled = false;
+            computerTurn();
+        }
+        public void restart()
+        {
+            foreach (TextBox tb in Controls.Cast<Control>().OfType<TextBox>())
+            {
+                tb.Text = "";
+                tb.Enabled = true;
+            }
+            label1.Text = "Click any square";
+            button1.Enabled = false;
+        }
+        public bool computerTurn()
+        {
+            List<Control> field = createField(); int cmpTurn;
+            if (checkWinner(field)) return true;
+
+            cmpCheckColumnsToBlock(field, "0", out cmpTurn);
+            if (cmpTurn == -1) cmpCheckRowsToBlock(field, "0", out cmpTurn);
+            if (cmpTurn == -1) cmpCheckDiagonalsToBlock(field, "0", out cmpTurn);
+
+            if (cmpTurn == -1) cmpCheckColumnsToBlock(field, "X", out cmpTurn);
+            if (cmpTurn == -1) cmpCheckRowsToBlock(field, "X", out cmpTurn);
+            if (cmpTurn == -1) cmpCheckDiagonalsToBlock(field, "X", out cmpTurn);
+            if (cmpTurn == -1) checkWhenNothingToBlock(field, "0", out cmpTurn);
+
+            if (field[4].Text == "" && i == 0)
+            {
+                cmpTurn = 4; i = 1;
+            }
+            //when it doesn't matter where to put "0" choose it randomly
+            if (cmpTurn == -1) cmpRandomChoice(field, out cmpTurn);           
+            field[cmpTurn].Text = "0"; field[cmpTurn].Enabled = false;
+            if (checkWinner(field)) return true;
+            return true;
+        }
+        private void cmpRandomChoice(List<Control> field, out int cmpTurn)
+        {         
+            while (true)
+            {
+                Random rnd = new Random();
+                cmpTurn = rnd.Next(0, 9);
+                if (field[cmpTurn].Text == "") break;
+            }          
+        }
+        private void cmpCheckColumnsToBlock(List<Control> field, string str, out int cmpTurn)
+        {
+            int qnt = 0, empty = -1;
+            cmpTurn = -1;
+            for (var l = 0; l < 3; l++)
+            {
+                for (var i = 0; i < 9; i += 3)
+                {
+                    if (field[i + l].Text == "") empty = i + l;
+                    if (field[i + l].Text == str) qnt++;
+                    
+                }
+                if (qnt == 2 && empty != -1)
+                {
+                    cmpTurn = empty;
+                    break;
+                }
+                else
+                {
+                    cmpTurn = -1;
+                    qnt = 0; empty = -1;
+                }
+            }
+        }
+        private void cmpCheckRowsToBlock(List<Control> field, string str, out int cmpTurn)
+        {
+            int qnt = 0, empty = -1; 
+            cmpTurn = -1;
+            for (var i = 0; i < 9; i += 3)
+            {
+                for (var l = 0; l < 3; l++)
+                {
+                    if (field[i + l].Text == "") empty = i + l;
+                    if (field[i + l].Text == str) qnt++;
+                }
+                if (qnt == 2 && empty != -1)
+                {
+                    cmpTurn = empty;
+                    break;
+                }
+                else
+                {
+                    cmpTurn = -1;
+                    qnt = 0; empty = -1; 
+                }
+            }
+        }
+        private void cmpCheckDiagonalsToBlock(List<Control> field, string str, out int cmpTurn)
+        {
+            int qnt = 0, empty = -1;
+            cmpTurn = -1;
+            for (var i = 0; i < 9; i += 4)
+            {
+                if (field[i].Text == "") empty = i;
+                if (field[i].Text == str) qnt++;
+              
+            }
+            if (qnt == 2 && empty != -1)
+            {
+                cmpTurn = empty;
+            }
+            else
+            {
+                qnt = 0; empty = -1;
+                for (var i = 2; i < 7; i += 2)
+                {
+                    if (field[i].Text == "") empty = i;
+                    if (field[i].Text == str) qnt++;                
+                }
+                if (qnt == 2 && empty != -1)
+                {
+                    cmpTurn = empty;
+                }
+                else
+                {
+                    cmpTurn = -1;
+                }
+            }
+        }
+        private void checkWhenNothingToBlock(List<Control> field, string str, out int cmpTurn)
+        {
+            int qnt = 0, empty = -1, e = 0;
+            cmpTurn = -1;
+            for (var i = 0; i < 9; i += 3)
+            {
+                for (var l = 0; l < 3; l++)
+                {
+                    if (field[i + l].Text == "") { empty = i + l; e++; }
+                    if (field[i + l].Text == str) qnt++;
+                }
+                if (e == 2 && qnt == 1)
+                {
+                    cmpTurn = empty; break;
+                }
+                else
+                {
+                    e = 0; qnt = 0; cmpTurn = -1;
+                }
+            }
+        }
+              
+        public List<Control> createField()
+        {
+            List<Control> field = new List<Control>();
+            foreach (TextBox tb in Controls.Cast<Control>().OfType<TextBox>())
+            {
+                field.Add(tb);
+            }
+            return field;
+        }
+        public void disableField()
+        {
+            foreach (TextBox tb in Controls.Cast<Control>().OfType<TextBox>())
+            {
+                tb.Enabled = false;              
+            }
+            button1.Enabled = true;
+        }
+        
+        public bool checkRows(List<Control> field)
+        {
+            for (var i = 0; i < 9; i += 3)
+            {
+                var l = i + 1; var k = i + 2;
+                if (field[i].Text == "X" && field[l].Text == "X" && field[k].Text == "X")
+                {
+                    label1.Text = "You won";
+                    return true;
+                }
+                else if (field[i].Text == "0" && field[l].Text == "0" && field[k].Text == "0")
+                {
+                    label1.Text = "Computer won"; 
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool checkColumns(List<Control> field)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                var l = i + 3; var k = i + 6;
+                if (field[i].Text == "X" && field[l].Text == "X" && field[k].Text == "X")
+                {
+                    label1.Text = "You won";
+                    return true;
+                }
+                else if (field[i].Text == "0" && field[l].Text == "0" && field[k].Text == "0")
+                {
+                    label1.Text = "Computer won";
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool checkDiagonals(List<Control> field)
+        {
+            if ((field[0].Text == "X" && field[4].Text == "X" && field[8].Text == "X")
+                || (field[2].Text == "X" && field[4].Text == "X" && field[6].Text == "X"))
+            {
+                label1.Text = "You won";
+                return true;
+            }
+            else if ((field[0].Text == "0" && field[4].Text == "0" && field[8].Text == "0")
+                || (field[2].Text == "0" && field[4].Text == "0" && field[6].Text == "0"))
+            {
+                label1.Text = "Computer won";
+                return true;
+            }
+            return false;
+        }
+
+        public bool checkWinner(List<Control> field)
+        {
+            if (checkRows(field) || checkColumns(field) || checkDiagonals(field))
+            {
+                disableField();
+                return true;
+            }
+            else
+            {
+                var num = 0;
+                for (var i = 0; i < 9; i++)
+                {
+                    if (field[i].Text == "") num++;
+                }
+                if (num == 0)
+                {
+                    label1.Text = "A draw";
+                    disableField();
+                    return true;
+                }
+            }
+            return false;
+        }                                                       
+    }
+}
